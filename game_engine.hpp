91d1a13aec11013c8c956a9ec79fc2424bf08b48
san-kir-k <dfomka@list.ru>
2020-12-21 16:21:52 +0300

    Add action

--- game_engine.hpp
+++ game_engine.hpp
@@ -4,13 +4,23 @@
 #include <ctime>
 
 namespace Seabattle {
-    const int MSIZE = 10; // размер поля
+    const int MSIZE = 10;
 
-    const int BATTLESHIP_C = 1; // характеристики кораблей
+    const int BATTLESHIP_C = 1;
     const int CRUISER_C = 2;
     const int DESTROYER_C = 3;
     const int BOAT_C = 4;
 
+    enum class cell_type {
+        battleship, 
+        cruiser, 
+        destroyer, 
+        boat, 
+        none,
+        dead,
+        shooted,
+        missed
+    };
     enum bin_flags {
         aa = 0b11111111,
         h4 = 0b10000000,
@@ -24,9 +34,11 @@ namespace Seabattle {
         uu = 0b00000000
     };
     enum class direction {
+        none = -1,
         left = 0,
         down = 1
     };
+
 
     const std::vector<std::vector<int> > v1_matrix =
     {{aa, aa, aa, ~v4, ~v4, ~v4},
@@ -398,6 +410,196 @@ namespace Seabattle {
             }
             std::vector<std::vector<int>> get_map() {
                 return _map;
+            }
+            void get_map_c(int** field) {
+                for (int i = 0; i < MSIZE; ++i) {
+                    for (int j = 0; j < MSIZE; ++j) {
+                        field[i][j] = _map[i][j];
+                    }
+                }
+            }
+    };
+
+    class Action {
+        private:
+            std::vector<std::vector<int>> _map;
+            struct cell_stats {
+                int health;
+                std::vector<std::pair<int, int>> coords;
+                cell_type type;
+                direction dir;
+            };
+            std::vector<std::vector<cell_stats>> _stats_map;
+            void _death_update(int row, int col, int len) {
+                for (auto p: _stats_map[row][col].coords) {
+                    _stats_map[p.first][p.second].type = cell_type::dead;
+                }
+                if (_stats_map[row][col].dir == direction::left) {
+                    int area_r = _stats_map[row][col].coords[0].first;
+                    int area_c = _stats_map[row][col].coords[0].second - 1;
+                    if (area_r >= 0 && area_c >= 0) {
+                        _stats_map[area_r][area_c].type = cell_type::missed;
+                    }
+                    area_r = _stats_map[row][col].coords[len - 1].first;
+                    area_c = _stats_map[row][col].coords[len - 1].second + 1;
+                    if (area_r < MSIZE && area_c > MSIZE) {
+                        _stats_map[area_r][area_c].type = cell_type::missed;
+                    }
+                    area_r = _stats_map[row][col].coords[0].first - 1;
+                    for (int i = 0; i < len + 2; ++i) {
+                        area_c = _stats_map[row][col].coords[0].second - 1 + i;
+                        if (area_r >= 0 && area_c >= 0 && area_r < MSIZE && area_c > MSIZE) {
+                            _stats_map[area_r][area_c].type = cell_type::missed;
+                        }
+                    }
+                    area_r = _stats_map[row][col].coords[0].first + 1;
+                    for (int i = 0; i < len + 2; ++i) {
+                        area_c = _stats_map[row][col].coords[0].second - 1 + i;
+                        if (area_r >= 0 && area_c >= 0 && area_r < MSIZE && area_c > MSIZE) {
+                            _stats_map[area_r][area_c].type = cell_type::missed;
+                        }
+                    }
+                } else {
+                    int area_r = _stats_map[row][col].coords[0].first - 1;
+                    int area_c = _stats_map[row][col].coords[0].second;
+                    if (area_r >= 0 && area_c >= 0) {
+                        _stats_map[area_r][area_c].type = cell_type::missed;
+                    }
+                    area_r = _stats_map[row][col].coords[len - 1].first + 1;
+                    area_c = _stats_map[row][col].coords[len - 1].second;
+                    if (area_r < MSIZE && area_c > MSIZE) {
+                        _stats_map[area_r][area_c].type = cell_type::missed;
+                    }
+                    area_c = _stats_map[row][col].coords[0].second - 1;
+                    for (int i = 0; i < len + 2; ++i) {
+                        area_r = _stats_map[row][col].coords[0].first - 1 + i;
+                        if (area_r >= 0 && area_c >= 0 && area_r < MSIZE && area_c > MSIZE) {
+                            _stats_map[area_r][area_c].type = cell_type::missed;
+                        }
+                    }
+                    area_c = _stats_map[row][col].coords[0].second + 1;
+                    for (int i = 0; i < len + 2; ++i) {
+                        area_r = _stats_map[row][col].coords[0].first - 1 + i;
+                        if (area_r >= 0 && area_c >= 0 && area_r < MSIZE && area_c > MSIZE) {
+                            _stats_map[area_r][area_c].type = cell_type::missed;
+                        }
+                    }
+                }
+            }
+            void _shoot_update(int row, int col) {
+                for (auto p: _stats_map[row][col].coords) {
+                    _stats_map[p.first][p.second].health--;
+                }
+                int len = 0;
+                if (_stats_map[row][col].type == cell_type::boat) {
+                    len = 1;
+                } else if (_stats_map[row][col].type == cell_type::destroyer) {
+                    len = 2;
+                } else if (_stats_map[row][col].type == cell_type::cruiser) {
+                    len = 3;
+                } else {
+                    len = 4;
+                }
+                _stats_map[row][col].type = cell_type::shooted;
+                if (_stats_map[row][col].health == 0) {
+                    _death_update(row, col, len);
+                }
+            }
+        public:
+            Action() = default;
+            Action(int** field) {
+                _map = std::vector<std::vector<int>>(MSIZE, std::vector<int> (MSIZE, 0));
+                _stats_map = std::vector<std::vector<cell_stats>>(MSIZE, std::vector<cell_stats> (MSIZE));
+                for (int i = 0; i < MSIZE; ++i) {
+                    for (int j = 0; j < MSIZE; ++j) {
+                        _map[i][j] = field[i][j];
+                        if (_map[i][j] == 0) {
+                            _stats_map[i][j].health = 0;
+                            _stats_map[i][j].type = cell_type::none;
+                            _stats_map[i][j].dir = direction::none;
+                        } else if (_map[i][j] == 1) {
+                            _stats_map[i][j].health = 1;
+                            _stats_map[i][j].type = cell_type::boat;
+                            _stats_map[i][j].dir = direction::none;
+                        } else if (_map[i][j] == 2) {
+                            _stats_map[i][j].health = 2;
+                            _stats_map[i][j].type = cell_type::destroyer;
+                            if ((i + 1 < MSIZE && _map[i + 1][j] == 2) ||
+                                (i - 1 >= 0 && _map[i - 1][j] == 2)) {
+                                _stats_map[i][j].dir = direction::down;
+                            } else if ((j + 1 < MSIZE && _map[i][j + 1] == 2) ||
+                                       (j - 1 >= 0 && _map[i][j - 1] == 2)) {
+                                _stats_map[i][j].dir = direction::left;
+                            }
+                        } else if (_map[i][j] == 3) {
+                            _stats_map[i][j].health = 3;
+                            _stats_map[i][j].type = cell_type::cruiser;
+                            if ((i + 1 < MSIZE && _map[i + 1][j] == 3) ||
+                                (i - 1 >= 0 && _map[i - 1][j] == 3)) {
+                                _stats_map[i][j].dir = direction::down;
+                            } else if ((j + 1 < MSIZE && _map[i][j + 1] == 3) ||
+                                       (j - 1 >= 0 && _map[i][j - 1] == 3)) {
+                                _stats_map[i][j].dir = direction::left;
+                            }
+                        } else {
+                            _stats_map[i][j].health = 4;
+                            _stats_map[i][j].type = cell_type::battleship;
+                            if ((i + 1 < MSIZE && _map[i + 1][j] == 4) ||
+                                (i - 1 >= 0 && _map[i - 1][j] == 4)) {
+                                _stats_map[i][j].dir = direction::down;
+                            } else if ((j + 1 < MSIZE && _map[i][j + 1] == 4) ||
+                                       (j - 1 >= 0 && _map[i][j - 1] == 4)) {
+                                _stats_map[i][j].dir = direction::left;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < MSIZE; ++i) {
+                    for (int j = 0; j < MSIZE; ++j) {
+                        if (_stats_map[i][j].type != cell_type::none && _stats_map[i][j].coords.size() != 0) {
+                            if (_stats_map[i][j].dir == direction::left) {
+                                int k = j;
+                                std::vector<std::pair<int, int>> tmp_coords;
+                                while ((k < MSIZE) && (k < (j + _stats_map[i][j].health))) {
+                                    tmp_coords.push_back({i, k});
+                                    k++;
+                                }
+                                k = j;
+                                while ((k < MSIZE) && (k < (j + _stats_map[i][j].health))) {
+                                    _stats_map[i][k].coords = tmp_coords;
+                                    k++;
+                                }
+                            } else {
+                                int k = i;
+                                std::vector<std::pair<int, int>> tmp_coords;
+                                while ((k < MSIZE) && (k < (i + _stats_map[i][j].health))) {
+                                    tmp_coords.push_back({k, j});
+                                    k++;
+                                }
+                                k = i;
+                                while ((k < MSIZE) && (k < (i + _stats_map[i][j].health))) {
+                                    _stats_map[k][j].coords = tmp_coords;
+                                    k++;
+                                }                                
+                            }
+                        }
+                    }
+                }
+            }
+            ~Action() = default;
+            void shoot(int row, int col) {
+                if (row >= MSIZE || col >= MSIZE || row < 0|| col < 0) {
+                    return;
+                }
+                if (_stats_map[row][col].type == cell_type::none) {
+                    _stats_map[row][col].type = cell_type::missed;
+                } 
+                if (_stats_map[row][col].type == cell_type::boat ||
+                    _stats_map[row][col].type == cell_type::destroyer ||
+                    _stats_map[row][col].type == cell_type::cruiser ||
+                    _stats_map[row][col].type == cell_type::battleship) {
+                    _shoot_update(row, col);
+                }
             }
     };
 }
